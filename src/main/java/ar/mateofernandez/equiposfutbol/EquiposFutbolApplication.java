09e82c39c515b9d18aa03fde3a2cf1e374cc3042
Mateo Fernandez <mateo.rost@gmail.com>
2024-04-17 17:39:20 -0300

    Inicio del proyecto

---
+++ src/main/java/ar/mateofernandez/equiposfutbol/EquiposFutbolApplication.java
@@ -0,0 +1,13 @@
+package ar.mateofernandez.equiposfutbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiposFutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquiposFutbolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/mateofernandez/equiposfutbol/EquiposFutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.mateofernandez.equiposfutbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquiposFutbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
